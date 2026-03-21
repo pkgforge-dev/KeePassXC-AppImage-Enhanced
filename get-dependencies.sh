b54407f26227ced28f2f1b824b4b05987d3073ea
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-20 23:08:44 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm keepassxc qt5ct kvantum-qt5
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
